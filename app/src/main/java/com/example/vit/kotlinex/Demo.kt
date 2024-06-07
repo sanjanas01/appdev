486c03c53ae9c48832d3f613fbb247b2bb4e6b81
SOS-5 <sanjana.s0304@gmail.com>
2024-06-07 10:34:47 +0530

    new commit

---
+++ app/src/main/java/com/example/vit/kotlinex/Demo.kt
@@ -0,0 +1,12 @@
+package com.example.vit.kotlinex
+
+fun main() {
+
+    val Fish = 50
+    val Plants = 23
+    if (Fish > Plants) {
+        println("Good ratio!")
+    } else {
+        println("Unhealthy ratio")
+    }
+}
