da36d1dd5085263e37992005f0fc88e678c4923f
SOS-5 <sanjana.s0304@gmail.com>
2024-06-08 10:32:33 +0530

    new commit

--- app/src/main/java/com/example/vit/MainActivity.kt
+++ app/src/main/java/com/example/vit/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.vit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun clickMethod(view: View) {
+        Log.i("MainActivity-click method","Next page")
     }
 }
