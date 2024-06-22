47a591e506849152d56fa2e527d6bcce4dcd086d
SOS-5 <sanjana.s0304@gmail.com>
2024-06-22 11:24:09 +0530

    viewing database

--- app/src/main/java/com/example/vit/HomeActivity.kt
+++ app/src/main/java/com/example/vit/HomeActivity.kt
@@ -7,7 +7,16 @@ import com.example.vit.database.ItemDAO
 import com.example.vit.database.Itemroomdb
 import com.example.vit.databinding.ActivityHomeBinding
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.last
+import kotlinx.coroutines.flow.single
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.last
+import kotlinx.coroutines.flow.single
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.firstOrNull
 
 class HomeActivity : AppCompatActivity() {
 
@@ -22,15 +31,33 @@ class HomeActivity : AppCompatActivity() {
         val database = Itemroomdb.getDatabase(this)
         dao = database.itemDao()
 
-        binding.btnDbInsert.setOnClickListener {
-            insertDataDb()
+        binding.insert.setOnClickListener {
+            insert1()
+        }
+
+        binding.view.setOnClickListener {
+            view(2)
         }
     }
 
-    private fun insertDataDb() {
+    private fun insert1() {
         GlobalScope.launch {
             val item = Item(name = "dosa", price = 99.99, quantity = 2)
             dao.insert(item)
         }
     }
+
+    private fun view(id: Int) {
+        GlobalScope.launch(Dispatchers.Main) {
+            val item = dao.getItem(id).firstOrNull() // Use firstOrNull() to handle null case
+            if (item != null) {
+                binding.tvHome.text = item.name
+            } else {
+                // Handle case where item with the given ID is not found
+                binding.tvHome.text = "Item not found"
+            }
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/vit/database/ItemDAO.kt
+++ app/src/main/java/com/example/vit/database/ItemDAO.kt
@@ -6,21 +6,23 @@ import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
+import androidx.room.OnConflictStrategy
+
 
 @Dao
 interface ItemDAO {
 
-    @Insert
+    @Insert (onConflict = OnConflictStrategy.REPLACE)                        //insert into item(.name,price,)
     suspend fun insert(groceryItem: Item)
 
     @Update
-    suspend fun update(item: Item)
+    suspend   fun update(item: Item)
 
     @Delete
     suspend fun delete(item: Item)
 
-    @Query("SELECT * from item WHERE id = :id")
-    fun getItem(id: Int): Flow<Item>
+    @Query("SELECT * from item WHERE id = :itemId")
+    fun getItem(itemId: Int): Flow<Item>
 
     @Query("SELECT * from item ORDER BY name ASC")
     fun getItems(): Flow<List<Item>>
